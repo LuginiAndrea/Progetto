eec5b7a14220392d125272598fbec63493ae965f
Andrea Lugini <lugiolugini@gmail.com>
2022-06-17 13:10:42 +0200

    ZXX

--- build/index.js
+++ build/index.js
@@ -22,42 +22,93 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 exports.__esModule = true;
 var app_1 = require("./app");
 var DB_interface_1 = require("./logic/db_interface/DB_interface");
 var email_1 = require("./logic/email/email");
 var admin = __importStar(require("firebase-admin"));
-app_1.app.listen(process.env.PORT || 8080, function () {
-    (0, email_1.send_generic_error_email)("Server started", "Server started");
-    try {
-        app_1.app.locals.DEFAULT_DB_INTERFACE = new DB_interface_1.DB_interface({
-            connectionString: (0, DB_interface_1.get_db_uri)()
-        }, true);
-    }
-    catch (error) {
-        (0, email_1.send_generic_error_email)("Error initializing database: ", error);
-    }
-    try { // Do this
-        var service_account = {
-            type: process.env.FIREBASE_TYPE,
-            project_id: process.env.FIREBASE_PROJECT_ID,
-            private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-            private_key: process.env.FIREBASE_PRIVATE_KEY,
-            client_email: process.env.FIREBASE_CLIENT_EMAIL,
-            client_id: process.env.FIREBASE_CLIENT_ID,
-            auth_uri: process.env.FIREBASE_AUTH_URI,
-            token_uri: process.env.FIREBASE_TOKEN_URI,
-            auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-            client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
-        }; //Has to be put as any or cert admin.credential.cert will throw type error
-        app_1.app.locals.FIREBASE_APP = admin.initializeApp({
-            credential: admin.credential.cert(service_account),
-            storageBucket: process.env.FIREBASE_BUCKET_URL
-        });
-    }
-    catch (error) {
-        console.log(error);
-        (0, email_1.send_generic_error_email)("Error initializing firebase: ", error);
-        process.exit(2);
-    }
-});
+app_1.app.listen(process.env.PORT || 8080, function () { return __awaiter(void 0, void 0, void 0, function () {
+    var error_1, service_account, error_2;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                _a.trys.push([0, 1, , 3]);
+                app_1.app.locals.DEFAULT_DB_INTERFACE = new DB_interface_1.DB_interface({
+                    connectionString: (0, DB_interface_1.get_db_uri)()
+                }, true);
+                return [3 /*break*/, 3];
+            case 1:
+                error_1 = _a.sent();
+                return [4 /*yield*/, (0, email_1.send_generic_error_email)("Error initializing database: ", error_1)];
+            case 2:
+                _a.sent();
+                process.exit(3);
+                return [3 /*break*/, 3];
+            case 3:
+                _a.trys.push([3, 4, , 6]);
+                service_account = {
+                    type: process.env.FIREBASE_TYPE,
+                    project_id: process.env.FIREBASE_PROJECT_ID,
+                    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+                    private_key: process.env.FIREBASE_PRIVATE_KEY,
+                    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+                    client_id: process.env.FIREBASE_CLIENT_ID,
+                    auth_uri: process.env.FIREBASE_AUTH_URI,
+                    token_uri: process.env.FIREBASE_TOKEN_URI,
+                    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+                    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
+                };
+                console.log(service_account.private_key);
+                app_1.app.locals.FIREBASE_APP = admin.initializeApp({
+                    credential: admin.credential.cert(service_account),
+                    storageBucket: process.env.FIREBASE_BUCKET_URL
+                });
+                return [3 /*break*/, 6];
+            case 4:
+                error_2 = _a.sent();
+                console.log(error_2);
+                return [4 /*yield*/, (0, email_1.send_generic_error_email)("Error initializing firebase: ", error_2)];
+            case 5:
+                _a.sent();
+                process.exit(2);
+                return [3 /*break*/, 6];
+            case 6: return [2 /*return*/];
+        }
+    });
+}); });
--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,15 @@ import { get_db_uri, DB_interface } from "./logic/db_interface/DB_interface";
 import { send_generic_error_email } from "./logic/email/email";
 import * as admin from "firebase-admin";
 
-app.listen(process.env.PORT || 8080, () => { // On start connect to Database
-    send_generic_error_email("Server started", "Server started");
+app.listen(process.env.PORT || 8080, async () => { // On start connect to Database
     try {
         app.locals.DEFAULT_DB_INTERFACE = new DB_interface({
             connectionString: get_db_uri()
         }, true);
     } 
     catch (error) {
-        send_generic_error_email("Error initializing database: ", error);
+        await send_generic_error_email("Error initializing database: ", error);
+        process.exit(3);
     }
     try { // Do this
         const service_account = {
@@ -26,6 +26,7 @@ app.listen(process.env.PORT || 8080, () => { // On start connect to Database
             auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
             client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
         } as any; //Has to be put as any or cert admin.credential.cert will throw type error
+        console.log(service_account.private_key);
         app.locals.FIREBASE_APP = admin.initializeApp({
             credential: admin.credential.cert(service_account),
             storageBucket: process.env.FIREBASE_BUCKET_URL
@@ -33,7 +34,7 @@ app.listen(process.env.PORT || 8080, () => { // On start connect to Database
     }
     catch(error) {
         console.log(error);
-        send_generic_error_email("Error initializing firebase: ", error);
+        await send_generic_error_email("Error initializing firebase: ", error);
         process.exit(2);
     }
 });
